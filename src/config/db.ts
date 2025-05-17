aada129a14aac848ece4949bb43af58d4297f9e7
claudioKabeli <claudio.flores@kabeli.cl>
2025-05-17 14:21:48 -0400

    fix:

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,23 +4,23 @@ import { Sequelize } from "sequelize";
 let pool: mysql.Pool;
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || "nsgps",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "123456",
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
-    logging: false, // puedes habilitarlo para ver las queries
+    logging: false,
   }
 );
 
 export const db = async () => {
   if (!pool) {
     pool = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD || "123456",
+      database: process.env.DB_NAME || "nsgps",
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
